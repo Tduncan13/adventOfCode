3a6a2a30e711b063e6b5daa098c68f285d0cbece
Tduncan13 <chico55@DESKTOP-Q1VDKNU.localdomain>
2021-12-02 10:33:07 -0500

    Day02 part 2 complete.

--- 2021/day02/python/part2.py
+++ 2021/day02/python/part2.py
@@ -6,13 +6,16 @@ def main(lines):
     m = {
         'forward': 0,
         'up': 0,
-        'down': 0
+        'down': 0,
+        'depth': 0
     }
     for line in lines:
         line = line.split()
+        if line[0] == 'forward':
+            m['depth'] = m['depth'] + (int(line[1]) * (m['down'] - m['up']))
         m[line[0]] = m[line[0]] + int(line[1])
 
-    return m['forward'] * (m['down'] - m['up'])
+    return m['forward'] * m['depth']
 
 
 if __name__ == '__main__':
